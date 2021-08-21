d6dbec9517bf554a0e58222e2547d3d5b6c7640f
Jose Manuel <castellanogonzalezjosemanuel@gmail.com>
2021-08-21 18:08:32 +0200

    deploy 2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route exact path="/home" component={Home} />
+          <Route exact path="/" component={Home} />
           <Route path="/list" component={List} />
         </Switch>
       </BrowserRouter>
